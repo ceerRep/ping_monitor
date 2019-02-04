d35cb81e3b5675a5d97669fe1873c42b78d5587e
ceerRep <ceerrep@gmail.com>
2019-02-04 17:38:13 +0800

    Added class WebServer

--- run.py
+++ run.py
@@ -3,17 +3,18 @@
 import threading
 
 from monitor import Monitor
-from webserver import runServer
+from webserver import WebServer
 
 import config
 
 if __name__ == "__main__":
     mon = Monitor(config.serverList, config.database_name)
 
-    def server(): return runServer('0.0.0.0', 8080)
+    server = WebServer(config.bind_addr, config.bind_port,
+                       config.database_name, config.serverList)
 
     thread_mon = threading.Thread(target=mon.loop)
-    thread_srv = threading.Thread(target=server)
+    thread_srv = threading.Thread(target=server.runServer)
 
     thread_mon.start()
     thread_srv.start()
--- webserver.py
+++ webserver.py
@@ -9,40 +9,50 @@ import config
 from database import database
 
 
-__app = Flask(__name__)
-__dbname = config.database_name
-__serverList = config.serverList
+class WebServer(object):
+    def __init__(self, bind_addr: str, bind_port: int, dbname: str, serverlst: list):
+        self.__app = Flask(__name__)
+        self.__bind_addr = bind_addr
+        self.__bind_port = bind_port
+        self.__dbname = dbname
+        self.__serverList = serverlst
 
+        @self.__app.route('/', methods=['GET', 'POST'])
+        def onGetRoot():
+            body = ''
+            with open('chart.html') as fin:
+                body = ''.join(fin.readlines())
+            return body
 
-@__app.route('/', methods=['GET', 'POST'])
-def onGetRoot():
-    body = ''
-    with open('chart.html') as fin:
-        body = ''.join(fin.readlines())
-    return body
+        @self.__app.route('/rawdata.json', methods=['GET'])
+        def onGetData():
+            db = database(self.__dbname)
+            dataset = dict()
+            for server in self.__serverList:
+                data = db.query_between(
+                    server[0], time.time() - 2 * 24 * 60 * 60, time.time())
+                dataset['%s <%s>' % (server[0], 'Private' if server[1] else server[3])] = [
+                    [time.asctime(time.localtime(row[0])), row[2]-row[4], row[2]+row[4], row[1], row[3]] for row in data]
+            db.close()
 
+            #start_response('200 OK', [('Content-Type', 'application/json')])
+            return (json.dumps(dataset), 201, {'Content-Type': 'application/json'})
 
-@__app.route('/rawdata.json', methods=['GET'])
-def onGetData():
-    db = database(__dbname)
-    dataset = dict()
-    for server in __serverList:
-        data = db.query_between(
-            server[0], time.time() - 2 * 24 * 60 * 60, time.time())
-        dataset['%s <%s>' % (server[0], 'Private' if server[1] else server[3])] = [
-            [time.asctime(time.localtime(row[0])), row[2]-row[4], row[2]+row[4], row[1], row[3]] for row in data]
-    db.close()
+        self.onGetRoot = onGetRoot
+        self.onGetData = onGetData
 
-    #start_response('200 OK', [('Content-Type', 'application/json')])
-    return (json.dumps(dataset), 201, {'Content-Type': 'application/json'})
-
-
-def runServer(addr: str, port: int):
-    __app.run(
-        host=addr,
-        port=port
-    )
+    def runServer(self):
+        self.__app.run(
+            host=self.__bind_addr,
+            port=self.__bind_port
+        )
 
 
 if __name__ == '__main__':
-    runServer('0.0.0.0', 8080)
+    server = WebServer(
+        '0.0.0.0',
+        8080,
+        config.database_name,
+        config.serverList
+    )
+    server.runServer()
